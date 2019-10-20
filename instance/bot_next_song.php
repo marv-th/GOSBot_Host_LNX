ccf02b2849cdc41ba3b1e9e19a95239a08410b27
unknown <marvman174@gmail.com>
2019-10-20 15:08:29 +0200

    fix some errors

--- instance/bot_next_song.php
+++ instance/bot_next_song.php
@@ -2,10 +2,6 @@
 
 $get_botid = $_SERVER['argv'][1];
 
-<<<<<<< HEAD
-echo "botid: $get_botid";
-=======
->>>>>>> 178d34b8bdcb2d141e2381322d10046952706f76
 ini_set("display_errors",0);
 
 $config_json = "/opt/gosbot/config/config.json";
@@ -17,12 +13,6 @@ $get_config_token = $json_conf["token"];
 if ($get_config_token==null) {
     exit("CONFIG TOKEN IS NOT SET");
 }
-<<<<<<< HEAD
-$url = 'https://gosbot.de/api?server_token='.$get_config_token.'&method=synch.bot.next.song&botID='.$get_botid.'';
-echo $url;
-$song_next = file_get_contents($url, false);
-=======
 
 $song_next = file_get_contents('https://gosbot.de/api?server_token='.$get_config_token.'&method=synch.bot.next.song&botID='.$get_botid.'', false, $context);
->>>>>>> 178d34b8bdcb2d141e2381322d10046952706f76
 echo $song_next;
--- instance/instance_stop.php
+++ instance/instance_stop.php
@@ -9,12 +9,6 @@ $conf = json_decode(file_get_contents($config_json), true);
 $conf["start"] = false;
 file_put_contents($config_json, json_encode($conf, JSON_PRETTY_PRINT));
 
-<<<<<<< HEAD
-$get_config_token = $conf["token"];
-$instance_power_url = 'https://gosbot.de/api/unix_token_check?token='.$get_config_token.'&method=instance.power.set.off&plrq=1';
-$instance_power = file_get_contents($instance_power_url, false);
-=======
->>>>>>> 178d34b8bdcb2d141e2381322d10046952706f76
 
 shell_exec(`cd /opt/gosbot/instance/bot && tmux send-keys -t "gosbot_client" '^C' C-m  >/dev/null 2>&1 && sleep 3 && tmux send-keys -t "gosbot_client" '^C' C-m  >/dev/null 2>&1 && sleep 1 && tmux kill-session -t "gosbot_client"  >/dev/null 2>&1`);
 echo "\nGOSBot Client wurde gestoppt.\n";
