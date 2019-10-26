15f4ee17e955935ce269cf481010aa7c75acf451
unknown <marvman174@gmail.com>
2019-10-26 18:00:49 +0200

    some changes

--- instance/instance_update_pr.php
+++ instance/instance_update_pr.php
@@ -1,6 +1,3 @@
 <?php 
 ini_set("display_errors",0);
-//shell_exec("cd /opt/gosbot/instance/ && php -f instance_update_se.php >/dev/null &2>/dev/null &");
-
 shell_exec('cd /opt/gosbot/instance/bot && tmux new -d -s "gosbot_host_update" php instance_update_se.php >/dev/null 2>&1');
-
